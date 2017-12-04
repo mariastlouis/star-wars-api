e234d431dc5cae43e500069bbb34895be57ef1d0
mariastlouis <msantray@yahoo.com>
2017-12-04 16:45:25 -0700

    Control component added

---
+++ src/Controls/Controls.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Controls = () => {
+  return (
+    <div className = "controls">
+      <p> Controls hooked up </p>
+    </div>
+  )
+}
+
+export default Controls;
