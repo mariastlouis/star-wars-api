36aeb6ddd4d972d2ea4c573870d5be6c5fdca797
mariastlouis <msantray@yahoo.com>
2017-12-06 19:55:48 -0700

    Change style on buttons

--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import './Controls.css'
 
 
 const Controls = ({clickCategory}) => {
   return (
     <div className = "controls">
        <button 
-          className="compare-button"
+          className="compare-button character"
            onClick={ () => clickCategory('character')}>
           People
         </button>
         <button 
-          className="compare-button"
+          className="compare-button character"
           onClick={ () => clickCategory('planet')}>
           Planets
         </button>
         <button 
-          className="compare-button"
+          className="compare-button character"
           onClick={ () => clickCategory('vehicle')}>
-          Planets
+          Vehicles
         </button>
 
 
