313af889a96d94f8188821f2f4a9901b485e8443
mariastlouis <msantray@yahoo.com>
2017-12-08 13:43:14 -0700

    Linter first pass complete

--- src/helper.js
+++ src/helper.js
@@ -1,8 +1,8 @@
 export const fetchMovieCrawl = async() =>{
-  const movieFetch = await fetch('https://swapi.co/api/films/')
-  const movieData = await movieFetch.json()
-  return fetchMovieData(movieData.results)
-}
+  const movieFetch = await fetch('https://swapi.co/api/films/');
+  const movieData = await movieFetch.json();
+  return fetchMovieData(movieData.results);
+};
 
 const fetchMovieData = (movieArray) => {
   return movieArray.map(movie => {
@@ -11,15 +11,15 @@ const fetchMovieData = (movieArray) => {
       releaseDate: movie.release_date,
       openingCrawl: movie.opening_crawl,
       id: movie.epsisode_id
-    }
-  })
-}
+    };
+  });
+};
 
 export const getVehicle = async() => {
-  const vehicleFetch = await fetch('https://swapi.co/api/vehicles/')
-  const vehicleData = await vehicleFetch.json()
-  return fetchVehicleData(vehicleData.results)
-}
+  const vehicleFetch = await fetch('https://swapi.co/api/vehicles/');
+  const vehicleData = await vehicleFetch.json();
+  return fetchVehicleData(vehicleData.results);
+};
 
 const fetchVehicleData = (vehicleData) => {
   return vehicleData.map(vehicle =>{
@@ -31,26 +31,25 @@ const fetchVehicleData = (vehicleData) => {
         Class: vehicle.vehicle_class,
         Passengers: vehicle.passengers
       }
-    }
-    })
-}
+    };
+  });
+};
 
 export const getPlanet = async () => {
-  const planetFetch = await fetch('https://swapi.co/api/planets/')
+  const planetFetch = await fetch('https://swapi.co/api/planets/');
   const planetData = await planetFetch.json();
   return fetchPlanetData(planetData.results);
-  // this.setState({planet})
-}
+};
 
 const fetchPlanetData = (planetData) => {
   const unresolvedPromises = planetData.map(async(planet) =>{
     const planetResidents = planet.residents;
     const residentPromises = planetResidents.map(async(resident) =>{
-      const residentFetch = await fetch (resident);
+      const residentFetch = await fetch(resident);
       const residentData = await residentFetch.json();
       return residentData.name;
     });
-    const residentNames = await Promise.all(residentPromises)
+    const residentNames = await Promise.all(residentPromises);
 
     return {
       name: planet.name,
@@ -62,37 +61,34 @@ const fetchPlanetData = (planetData) => {
         
       }
 
-    }
-
-  })
+    };
+  });
   return Promise.all(unresolvedPromises);
-}
+};
 
 export const getCharacter = async () => {
-  const peoplelFetch = await fetch('https://swapi.co/api/people/')
-  const peopleData  = await peoplelFetch.json()
+  const peoplelFetch = await fetch('https://swapi.co/api/people/');
+  const peopleData  = await peoplelFetch.json();
   return fetchPlanetSpecies(peopleData.results);
-   
-}
+};
 
 const fetchPlanetSpecies = (peopleData) => {
-    const unresolvedPromises = peopleData.map(async(character) => {
-      let homeworldFetch = await fetch(character.homeworld)
-      let homeworldData = await homeworldFetch.json()
-      let speciesFetch = await fetch(character.species)
-      let speciesData = await speciesFetch.json()
+  const unresolvedPromises = peopleData.map(async(character) => {
+    let homeworldFetch = await fetch(character.homeworld);
+    let homeworldData = await homeworldFetch.json();
+    let speciesFetch = await fetch(character.species);
+    let speciesData = await speciesFetch.json();
 
-      return {
-        name: character.name,
-        favorite: false,
-        data: {
-          Species: speciesData.name,
-          Language: speciesData.language,
-          Homeworld: homeworldData.name,
-          Population: homeworldData.population,
-        }
+    return {
+      name: character.name,
+      favorite: false,
+      data: {
+        Species: speciesData.name,
+        Language: speciesData.language,
+        Homeworld: homeworldData.name,
+        Population: homeworldData.population
       }
-    });
-
-    return Promise.all(unresolvedPromises)
-  }
+    };
+  });
+  return Promise.all(unresolvedPromises);
+};
