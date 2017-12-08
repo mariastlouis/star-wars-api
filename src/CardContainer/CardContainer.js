0dd807a831c35252740dc88540de6330af76bcf8
mariastlouis <msantray@yahoo.com>
2017-12-08 13:32:18 -0700

    Controls linter passing

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from '../Card/Card.js'
+import Card from '../Card/Card.js';
 import PropTypes from 'prop-types';
 import './CardContainer.css';
 
@@ -9,22 +9,22 @@ const CardContainer = ({data, addFavorite, category}) => {
   return (
     <div className = "card-container">
       <div className = "card-holder">
-      {
-        data.map((stat, index) =>{
-          return (
-            <Card
-            key = {index}
-            name = {stat.name}
-            data = {stat.data}
-            favorite = {stat.favorite} 
-            addFavorite = {addFavorite}
-            category = {category} />
+        {
+          data.map((stat, index) =>{
+            return (
+              <Card
+                key = {index}
+                name = {stat.name}
+                data = {stat.data}
+                favorite = {stat.favorite} 
+                addFavorite = {addFavorite}
+                category = {category} />
             );
-        })
-      }
+          })
+        }
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CardContainer;
--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -1,72 +1,66 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
-import './Controls.css'
+import './Controls.css';
 
 
 export default class Controls extends React.Component {
 
-constructor () {
-  super ();
-  this.state = {active: 0}
-  this.passClickInfo = this.passClickInfo.bind(this)
-
-}
-
-
-
-passClickInfo (category, position) {
-
-  this.props.clickCategory(category);
-  this.toggle(position);
-}
-
-toggle(position) {
-  if(this.state.active === position) {
-    this.setState({active : null})
-  } else {
-    this.setState({active: position})
+  constructor() {
+    super();
+    this.state = {active: 0};
+    this.passClickInfo = this.passClickInfo.bind(this);
   }
-}
+
+  passClickInfo (category, position) {
+    this.props.clickCategory(category);
+    this.toggle(position);
+  }
+
+  toggle(position) {
+    if (this.state.active === position) {
+      this.setState({active : null});
+    } else {
+      this.setState({active: position});
+    }
+  }
 
   bgColor(position) {
-    if(this.state.active === position) {
+    if (this.state.active === position) {
       return "#feda4a";
     } return "";
   }
 
 
-render(){
-  const favoriteNumber = this.props.favorites.length
-  return (
-    <div className = "controls">
-       <button 
+  render(){
+    const favoriteNumber = this.props.favorites.length;
+    return (
+      <div className = "controls">
+        <button 
           style = {{backgroundColor: this.bgColor(0)}}
           className="compare-button character"
-           onClick={ () => this.passClickInfo('character', 0)}>
-          People
+          onClick={ () => this.passClickInfo('character', 0)}>
+            People
         </button>
         <button
           style = {{backgroundColor: this.bgColor(1)}} 
           className="compare-button planet"
           onClick={ () => this.passClickInfo('planet', 1)}>
-          Planets
+            Planets
         </button>
         <button
           style = {{backgroundColor: this.bgColor(2)}}
           className="compare-button vehicle"
           onClick={ () => this.passClickInfo('vehicle', 2)}>
-          Vehicles
+            Vehicles
         </button>
         <button
           style = {{backgroundColor: this.bgColor(3)}}
           className="compare-button favorite"
           onClick={ () => this.passClickInfo('favorite', 3)}>
-          Favorites: {favoriteNumber}
+            Favorites: {favoriteNumber}
         </button>
-
-
-    </div>
-  )
-}
+      </div>
+    );
+  }
 }
 
