3c889c50703c36871048fa5fff1ab8812ee5e0ec
mariastlouis <msantray@yahoo.com>
2017-12-10 14:06:49 -0700

    Api fetch tests running

---
+++ src/helper/_mocks_/mockApiResults.js
@@ -0,0 +1,12 @@
+export const getCharacter = async () => {
+  return await ['Luke Skywalker', 'C-3PO']
+}
+
+export const getFilm = async () =>{
+  return await [{
+    title: 'A new hope',
+    date: 'May 15, 1977',
+    openingCrawl: 'movie crawl data',
+    id: 5
+  }]
+}
