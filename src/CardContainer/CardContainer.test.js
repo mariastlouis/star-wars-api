e7f22ff2f27bf13bd48f85da72494d8772a3e000
mariastlouis <msantray@yahoo.com>
2017-12-09 21:53:06 -0700

    Card container tests hooked up

---
+++ src/CardContainer/CardContainer.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import CardContainer from './CardContainer.js';
+import {shallow, mount} from 'enzyme';
+
+const mockAddFavorite = jest.fn();
+
+const mockVehicleInfo = [
+    {
+      name: 'Sand Crawler',
+      favorite: false,
+      info: {
+        Class: 'Wheeled',
+        Model: 'Digger Crawler',
+        Passengers: '30'
+      }
+    },
+    {
+      name: "X-34 landspeeder",
+      favorite: true,
+      info: {
+        Class: "repulsorcraft",
+        Model: "X-34 landspeeder",
+        Passengers: "1"
+      }
+
+    }
+  ]
+
+  const mockCategory = 'vehicle'
+
+
+let wrapper;
+
+describe ('Card Container', () => {
+  beforeEach(() => {
+   
+    wrapper = shallow (
+      <CardContainer 
+      category = {mockCategory}
+      info = {mockVehicleInfo}
+      addFavorite = {mockAddFavorite}
+    />);
+  });
+
+it('should render correctly', () =>{
+  expect(wrapper).toBeDefined();
+});
+
+it('should match the snapshot', () =>{
+  expect(wrapper).toMatchSnapshot();
+  console.log(wrapper.debug())
+})
+
+it('should render the number of cards for the length of the array given', () =>{
+  expect(wrapper.find('Card').length).toEqual(2);
+})
+
+});
