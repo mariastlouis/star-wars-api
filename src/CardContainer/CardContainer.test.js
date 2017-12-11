e2512b625eeafe5e673b57f6e8f2094635a2d5c9
mariastlouis <msantray@yahoo.com>
2017-12-10 18:40:54 -0700

    Card container test passing linter

--- src/CardContainer/CardContainer.test.js
+++ src/CardContainer/CardContainer.test.js
@@ -1,57 +1,55 @@
 import React from 'react';
 import CardContainer from './CardContainer.js';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 const mockAddFavorite = jest.fn();
 
 const mockVehicleInfo = [
-    {
-      name: 'Sand Crawler',
-      favorite: false,
-      info: {
-        Class: 'Wheeled',
-        Model: 'Digger Crawler',
-        Passengers: '30'
-      }
-    },
-    {
-      name: "X-34 landspeeder",
-      favorite: true,
-      info: {
-        Class: "repulsorcraft",
-        Model: "X-34 landspeeder",
-        Passengers: "1"
-      }
-
+  {
+    name: 'Sand Crawler',
+    favorite: false,
+    info: {
+      Class: 'Wheeled',
+      Model: 'Digger Crawler',
+      Passengers: '30'
     }
-  ]
+  },
+  {
+    name: "X-34 landspeeder",
+    favorite: true,
+    info: {
+      Class: "repulsorcraft",
+      Model: "X-34 landspeeder",
+      Passengers: "1"
+    }
+  }
+];
 
-  const mockCategory = 'vehicle'
-
+const mockCategory = 'vehicle';
 
 let wrapper;
 
-describe ('Card Container', () => {
+describe('Card Container', () => {
   beforeEach(() => {
    
-    wrapper = shallow (
+    wrapper = shallow(
       <CardContainer 
-      category = {mockCategory}
-      info = {mockVehicleInfo}
-      addFavorite = {mockAddFavorite}
-    />);
+        category = {mockCategory}
+        info = {mockVehicleInfo}
+        addFavorite = {mockAddFavorite}
+      />);
   });
 
-it('should render correctly', () =>{
-  expect(wrapper).toBeDefined();
-});
+  it('should render correctly', () =>{
+    expect(wrapper).toBeDefined();
+  });
 
-it('should match the snapshot', () =>{
-  expect(wrapper).toMatchSnapshot();
-})
+  it('should match the snapshot', () =>{
+    expect(wrapper).toMatchSnapshot();
+  });
 
-it('should render two cards if the length of the array passed in is two', () =>{
-  expect(wrapper.find('Card').length).toEqual(2);
-})
+  it('should render 2cards if the array length is 2', () =>{
+    expect(wrapper.find('Card').length).toEqual(2);
+  });
 
 });
